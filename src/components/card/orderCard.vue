64fd4bd10839ecf512bbdfbc01aad89c66c5e273
Agry88 <rgok307085@gmail.com>
2023-12-17 15:14:09 +0800

    feat: modify clientOrderPage.

--- src/components/card/orderCard.vue
+++ src/components/card/orderCard.vue
@@ -15,34 +15,34 @@ const handlearrow = () => showDetail.value ? "h-5 w-5 rotate-180" : "h-5 w-5"
 </script>
 <template>
     <div class="flex flex-col p-5 m-3 bg-graybg">
-        <div class="flex flex-row justify-between ">
-
-            <div class=" flex flex-row">
-
-                <img src="" alt="1234">
-                <div class="inline-flex flex-col gap-2">
-                    <h2 class="text-xl font-semibold">王先生</h2>
-                    <p class=" text-sm text-gray-400">於8月23日 PM2:13 點餐</p>
-                    <p>1x 黑降牛雞套餐</p>
-                    <p>1x 黑降牛雞套餐</p>
-                    <p>1x 黑降牛雞套餐</p>
-                </div>
+        <div class="flex flex-row justify-between py-4">
+          <img class="aspect-[4/3] w-1/3" src="https://picsum.photos/400/300?random=1" alt="1234">
+          <div class="inline-flex flex-col w-2/3 gap-2 px-4">
+            <div class="flex justify-between w-full">
+              <h2 class="text-xl font-semibold">王先生</h2>
+              <span>
+                  $413
+              </span>
             </div>
             <div>
-                $413
+              <p class="text-sm text-gray-400 ">於8月23日 PM2:13 點餐</p>
+              <p>1x 黑降牛雞套餐</p>
+              <p>1x 黑降牛雞套餐</p>
+              <p>1x 黑降牛雞套餐</p>
             </div>
+          </div>
         </div>
         <div class="border border-grayline border-b-1"></div>
-        <button @click="handleDetail" class=" flex flex-row justify-between m-4">
-            <p>查看細節</p>
+        <button @click="handleDetail" class="flex flex-row justify-between m-4 ">
+            <p class="text-xl font-bold">查看細節</p>
             <img :src="arrow" alt="arrow" :class="handlearrow()">
         </button>
         <div v-if="showDetail">
-            <div class=" m-5">
-                <div class=" flex flex-row justify-between">
+            <div class="m-5 ">
+                <div class="flex flex-row justify-between ">
                     <div>
                         <p class="text-xl font-semibold">1x 黑降牛雞套餐</p>
-                        <div class=" inline-flex flex-row">
+                        <div class="inline-flex flex-row ">
                             <p class="text-sm text-gray-400">
                                 （Ｍ）七喜
                             </p>
@@ -53,11 +53,11 @@ const handlearrow = () => showDetail.value ? "h-5 w-5 rotate-180" : "h-5 w-5"
                     </div>
                     <p>$413</p>
                 </div>
-                <div class=" flex flex-row justify-between">
+                <div class="flex flex-row justify-between ">
                     <div>
 
                         <p class="text-xl font-semibold">1x 黑降牛雞套餐</p>
-                        <div class=" inline-flex flex-row">
+                        <div class="inline-flex flex-row ">
                             <p class="text-sm text-gray-400">
                                 （Ｍ）七喜
                             </p>
@@ -71,12 +71,12 @@ const handlearrow = () => showDetail.value ? "h-5 w-5 rotate-180" : "h-5 w-5"
 
             </div>
             <div class="border border-grayline border-b-1"></div>
-            <div class=" inline-flex flex-row justify-between w-full">
+            <div class="inline-flex flex-row justify-between w-full ">
                 <p class="text-xl font-semibold">總計</p>
                 <p class="text-xl font-semibold">$826</p>
             </div>
-            <div class=" inline-flex justify-center w-full">
-                <button v-if="props.handleButton" class="  bg-gray-700 text-white p-2 w-96 text-xl font-semibold justify-center">
+            <div class="inline-flex justify-center w-full ">
+                <button v-if="props.handleButton" class="justify-center p-2 text-xl font-semibold text-white bg-gray-700 w-96">
                     通知客人取餐
                 </button>
             </div>
--- src/views/restaurants/ClientOrdersPage.vue
+++ src/views/restaurants/ClientOrdersPage.vue
@@ -3,27 +3,37 @@ import ClientHeader from '../../components/ClientHeader.vue';
 import OrderCard from '../../components/card/orderCard.vue';
 import { ref } from 'vue'
 const showButton = ref(false)
+
 const handleButton = () => {
     showButton.value = !showButton.value
 }
+
 </script>
 <template>
     <div class="flex flex-col items-center w-screen h-ful">
         <div class="w-full max-w-3xl ">
             <ClientHeader/>
+
             <div>
-                我的訂單
+              <h2 class="text-2xl font-bold">我的訂單</h2>
+              <div>
+                <OrderCard/>
+              </div>
             </div>
-            <OrderCard/>
+
             <div>
-                正在處理的訂單
+              <h2 class="text-2xl font-bold">正在處理的訂單</h2>
+              <div>
+                <OrderCard/>
+              </div>
             </div>
-            <OrderCard :handleButton="handleButton"/>
+
             <div>
-                
-                過往訂單
+              <h2 class="text-2xl font-bold">過往訂單</h2>
+              <div>
+                <OrderCard/>
+              </div>
             </div>
-            <OrderCard/>
         </div>
     </div>
 </template>
